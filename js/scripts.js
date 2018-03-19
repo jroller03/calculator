124994ff0dd6d336004589a838d96ae51dcce944
Kristi Hwang and Noah Medoff <kristi@>
2018-03-19 10:01:24 -0700

    Created add function

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function add(num1, num2){
+return num1+ num2;
+}
+
+var num1 = parseInt(prompt("Enter a number please"));
+var num2 = parseInt(prompt("Enter another number please"));
+alert(add(num1,num2));
