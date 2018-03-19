734ac5f88b60d89e8525ea54dc993339d9eb93ca
Kristi Hwang and Noah Medoff <kristi@>
2018-03-19 10:05:48 -0700

    Created fucntions for subtraxction, division, multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,21 @@ function add(num1, num2){
 return num1+ num2;
 }
 
+function subtract(num1, num2){
+  return num1 - num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function divide(num1, num2){
+  return num1 / num2;
+}
+
 var num1 = parseInt(prompt("Enter a number please"));
 var num2 = parseInt(prompt("Enter another number please"));
 alert(add(num1,num2));
+alert(subtract(num1, num2));
+alert(multiply(num1, num2));
+alert(divide(num1, num2));
